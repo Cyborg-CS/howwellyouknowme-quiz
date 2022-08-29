8af60748ef338eeb5a39431b899c4c99b1eb5441
devMainak <81987904+Cyborg-CS@users.noreply.github.com>
2022-08-29 15:29:15 +0000

    refactor: added indications

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const readlineSync = require("readline-sync");
 
 
 var userName = readlineSync.question("May I have your name ? ");
-console.log("Welcome to the game HOW GOOD YOU KNOW, Me ? "+ userName);
+console.log("Welcome to the game HOW GOOD YOU KNOW, Me ? " + userName);
 
-if( readlineSync.keyInYN("Would you like to play the game ? Then press 'y', if not press 'n'")){
+if (readlineSync.keyInYN("Would you like to play the game ? Then press 'y', if not press 'n'")) {
   console.log("Lets play then !")
-}else{
+  console.log("Answer by typing 'yes'/'no'")
+} else {
   console.log("Till next time then...")
   process.exit(1)
 }
@@ -17,62 +18,69 @@ var score = 0;
 var highestScorers = [
   {
     name: "neil",
-    score:"4"
-  },{
+    score: "4"
+  }, {
     name: "sandy",
     score: "3"
   }
 ]
 
-function play(question,answer){
+function play(question, answer) {
 
 
-var userAnswer = readlineSync.question(question);
+  var userAnswer = readlineSync.question(question);
 
-if (userAnswer === answer){
-  console.log("Right Answer !")
-  score++;
-}else{
-  console.log("Not Quite.")
-  score = score-1 ;
-}
+  if (userAnswer === answer) {
+    console.log("Right Answer !")
+    score++;
+  } else {
+    console.log("Not Quite.")
+    score = score - 1;
+  }
 
-console.log("Score: "+score);
-console.log("-------------")
+  console.log("Score: " + score);
+  console.log("-------------")
 }
 
 var questions = [
   {
-    question :"Am I 18 yet ? ",
-    answer : "yes"},
-  
-  
-    {question : "Is CR7 is my fav footballer ? ",
-    answer : "yes"},{
-      question:"Do I have a laptop ?",
-      answer: "no"
-    },
-  
-    {question : "Do I watch anime ? ",
-    answer : "yes"
-  },{
-    question: "Which is my Fav web series ? ",
-    answer : "Peaky Blinders"
+    question: "Am I 18 yet ? ",
+    answer: "yes"
+  },
+
+
+  {
+    question: "Is CR7 is my fav footballer ? ",
+    answer: "yes"
+  }, {
+    question: "Do I have a laptop ?",
+    answer: "no"
+  },
+
+  {
+    question: "Do I watch anime ? ",
+    answer: "yes"
+  }, {
+    question: "Is Peaky Blinders my Fav web series ? ",
+    answer: "yes"
+  }, {
+    question: "Is my fav anime Attack On Titan ?",
+    answer: "no"
   }
 
 ]
 
 
 
-for(i=0; i<questions.length; i++){
+for (i = 0; i < questions.length; i++) {
   var currentQuestion = questions[i];
-  play(currentQuestion.question,currentQuestion.answer)
-  }  
-  console.log("Your Final Score is "+score)
+  play(currentQuestion.question, currentQuestion.answer)
+}
+console.log("Your Final Score is " + score)
 
 
-for(i=0; i<highestScorers.length; i++){
+for (i = 0; i < highestScorers.length; i++) {
   var currentScorer = highestScorers[i];
   console.log("Here are the Highest scorers - ")
-  console.log(currentScorer.name,currentScorer.score);
+  console.log(currentScorer.name, currentScorer.score);
 }
